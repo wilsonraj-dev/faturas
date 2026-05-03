f6ae16400e06d2b85542665df3a76c575f055366
Wilson Junior <wilson.assis.junior@gmail.com>
2026-05-03 15:04:17 -0300

    Criação e aplicação do migration no banco

--- Fatura.Server/Migrations/20260503180247_ModuloFinanceiro.Designer.cs
+++ Fatura.Server/Migrations/20260503180247_ModuloFinanceiro.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Fatura.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fatura.Server.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260503180247_ModuloFinanceiro")]
+    partial class ModuloFinanceiro
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
